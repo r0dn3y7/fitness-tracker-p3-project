51aed5d4eabbebe0c89fa926556f0695f8278dba
Rodney <rodney.amani@student.moringaschool.com>
2025-05-27 10:07:43 +0300

    complete

--- fitness_tracker/lib/models/user.py
+++ fitness_tracker/lib/models/user.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from . import Base
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}', age={self.age})>"
+    
