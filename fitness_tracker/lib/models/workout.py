3301b263dfc751841ea4649cf607c07b38b0ebc7
Rodney <rodney.amani@student.moringaschool.com>
2025-05-29 18:25:05 +0300

    workout changes

--- fitness_tracker/lib/models/workout.py
+++ fitness_tracker/lib/models/workout.py
@@ -1,17 +1,14 @@
-# lib/models/workout.py
-
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from fitness_tracker.lib.models import Base
+from models.__init__ import Base  
 
 class Workout(Base):
-    __tablename__ = 'workouts'
+    __tablename__ = "workouts"
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey("users.id"))
 
     user = relationship("User", back_populates="workouts")
-    exercises = relationship("Exercise", back_populates="workout", cascade="all, delete-orphan")
 
     def __repr__(self):
-        return f"<Workout(id={self.id}, description='{self.description}')>"
+        return f"<Workout(id={self.id}, user_id={self.user_id})>"
