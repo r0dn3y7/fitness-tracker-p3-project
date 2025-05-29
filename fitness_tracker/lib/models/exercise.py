6af93bb49deb6067cbc46a635cbc786af0d9535a
Rodney <rodney.amani@student.moringaschool.com>
2025-05-29 17:35:25 +0300

    exercise

---
+++ fitness_tracker/lib/models/exercise.py
@@ -0,0 +1,18 @@
+# lib/models/exercise.py
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from lib.models import Base
+
+class Exercise(Base):
+    __tablename__ = 'exercises'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    reps = Column(Integer)
+    workout_id = Column(Integer, ForeignKey('workouts.id'))
+
+    workout = relationship("Workout", back_populates="exercises")
+
+    def __repr__(self):
+        return f"<Exercise(name='{self.name}', reps={self.reps})>"
