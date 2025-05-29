7f6d7955f596ed55997d0fe94b5f7f7e035449a6
Rodney <rodney.amani@student.moringaschool.com>
2025-05-29 18:55:15 +0300

    changes

--- fitness_tracker/lib/helpers.py
+++ fitness_tracker/lib/helpers.py
@@ -1,8 +1,5 @@
 from fitness_tracker.lib.models.user import User
-from fitness_tracker.lib.models.workout import Workout
-from fitness_tracker.lib.models.exercise import Exercise
-from fitness_tracker.lib.models import session  
-
+from fitness_tracker.lib.models import session
 
 def create_user():
     name = input("Enter the user's name: ")
@@ -13,13 +10,20 @@ def create_user():
 
 def list_users():
     users = session.query(User).all()
+    if not users:
+        print("No users found.")
     for user in users:
-        print(user)
+        print(f"ID: {user.id}, Name: {user.name}")
 
 def delete_user():
     list_users()
-    user_id = input("Enter the ID of the user to delete: ")
-    user = session.query(User).get(user_id)
+    try:
+        user_id = int(input("Enter the ID of the user to delete: "))
+    except ValueError:
+        print("Please enter a valid number.")
+        return
+
+    user = session.get(User, user_id)
     if user:
         session.delete(user)
         session.commit()
