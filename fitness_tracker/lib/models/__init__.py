a5aa224673250882fb0d86a35975fce70ccc6d84
Rodney <rodney.amani@student.moringaschool.com>
2025-05-29 18:49:55 +0300

    changes

--- fitness_tracker/lib/models/__init__.py
+++ fitness_tracker/lib/models/__init__.py
@@ -1,12 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from fitness_tracker.lib.models import Base
-from fitness_tracker.lib.models.user import User
-from fitness_tracker.lib.models.workout import Workout
-from fitness_tracker.lib.models.exercise import Exercise
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
 
 engine = create_engine('sqlite:///fitness_tracker.db')
 Session = sessionmaker(bind=engine)
 session = Session()
-
-Base.metadata.create_all(engine) 
